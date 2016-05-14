23a835a5df24e49f1e74be40f4f0b9e7f72fdf03
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-05-14 20:22:54 +0000

    Added validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,14 @@ def new
 end
 
 def create
-  current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
   redirect_to root_path
+else
+  render :new, status:   :unprocessable_entity
 end
+end
+
 
 def show
   @place = Place.find(params[:id])
@@ -33,8 +38,13 @@ def update
 end
 
   @place.update_attributes(place_params)
+  if @place.valid?
    redirect_to root_path
+  else
+    render  :edit, status:  :unprocessable_entity
 end
+end
+
 
 def destroy
 @place = Place.find(params[:id])
