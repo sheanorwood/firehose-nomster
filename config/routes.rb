83671b4761a602c42bdbc0d50009c526c9a59ac5
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-05-15 02:47:43 +0000

    Making the submit button save the comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Nomster::Application.routes.draw do
 
 devise_for :users
-resources :places
-
 root 'places#index'
+
+resources :places do
+      resources  :comments, only:  :create
+end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
