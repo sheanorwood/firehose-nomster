ccc10e17d14a3f5b7a44f372d9f903a88fa29589
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-05-18 04:40:59 +0000

    Tweaking Automated Emails

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
     belongs_to  :user
     belongs_to  :place
+    after_create :send_comment_email
 
 RATINGS = {
 
@@ -13,6 +14,11 @@ RATINGS = {
 
 def humanized_rating
     RATINGS.invert[self.rating]
-  end
+end
+
+def send_comment_email
+       NotificationMailer.comment_added(self).deliver
+end
+
 
 end
