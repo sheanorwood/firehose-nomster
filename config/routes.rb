6dba98b0e1e5f1434d82defd2da08af09ff34d16
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-05-19 01:15:23 +0000

    Add user profile link

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ devise_for :users
 root 'places#index'
 
 resources  :places do
-resources  :comments, only:  :create
+    resources  :comments, only:  :create
 resources  :photos, only:  :create
-
 end
+
+resources  :users, only:  :show
 
 
 
